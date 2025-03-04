5a85f469f8cda4b7ed4e72574c45b51f5b1091cb
Adrien Chew <74026675+AdrienChew246@users.noreply.github.com>
2025-03-03 22:05:51 -0600

    Initial Commit

---
+++ Touch Grass/Touch_GrassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_GrassApp.swift
+//  Touch Grass
+//
+//  Created by Adrien Chew on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Touch_GrassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Touch GrassTests/Touch_GrassTests.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_GrassTests.swift
+//  Touch GrassTests
+//
+//  Created by Adrien Chew on 3/3/25.
+//
+
+import Testing
+@testable import Touch_Grass
+
+struct Touch_GrassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
