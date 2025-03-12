5d219ccf3133338729a47b43c0a35a09878aa568
Adrien Chew <74026675+AdrienChew246@users.noreply.github.com>
2025-03-12 17:39:38 -0500

    Added working progress bar

--- Touch Grass/ContentView.swift
+++ Touch Grass/ContentView.swift
@@ -8,15 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var percent: CGFloat = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("TOUCH GRASS!")
+            Text("\(Int(percent))%")
+                .font(.system(size: 72, weight: .bold, design: .default))
             
+            ProgressBar(width: 300, height: 30, percent: percent, baseColor:Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.09622130102)), barColor:Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)))
+                .animation(.bouncy)
+            
+            ZStack {
+                Rectangle()
+                    .frame(width: 250, height: 30)
+                    .foregroundColor(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
+                
+                HStack {
+                    Button (action: { percent = percent - 5 },
+                            label: { Text("Decrease") })
+                    
+                    Button(action: { percent = CGFloat.random(in: 0...100)},
+                           label: { Text("Randomize") })
+                    
+                    Button (action: { percent = percent + 5 },
+                            label: { Text("Increase") })
+                }
+            }
         }
-        .padding()
+        .padding(.top, -350)
     }
 }
 
---
+++ Touch Grass/ProgressBar.swift
@@ -0,0 +1,34 @@
+//
+//  ProgressBar.swift
+//  Touch Grass
+//
+//  Created by Adrien Chew on 3/12/25.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    var width: CGFloat = 200
+    var height: CGFloat = 20
+    var percent: CGFloat = 50
+    var baseColor = Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.1))
+    var barColor = Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1))
+    
+    
+    var body: some View {
+        let multiplier = width / 100
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: height, style: .continuous)
+                .frame(width: width, height: height)
+                .foregroundColor(baseColor)
+            
+            RoundedRectangle(cornerRadius: height, style: .continuous)
+                .frame(width: percent * multiplier, height: height)
+                .foregroundColor(barColor)
+        }
+    }
+}
+
+#Preview {
+    ProgressBar()
+}
