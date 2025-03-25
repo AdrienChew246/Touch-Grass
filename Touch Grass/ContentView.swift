8f557452e70fd7ff4f1bb6e9d757f37c4ee7d4ab
Adrien Chew <74026675+AdrienChew246@users.noreply.github.com>
2025-03-25 16:34:16 -0500

    Bluetooth attempt 1

--- Touch Grass/ContentView.swift
+++ Touch Grass/ContentView.swift
@@ -6,25 +6,59 @@
 //
 
 import SwiftUI
+import CoreBluetooth
+
+// Bluetooth stuff
+class BluetoothViewModel: NSObject, ObservableObject {
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral] = []
+    @Published var peripheralNames: [String] = []
+    
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+    }
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            self.peripherals.append(peripheral)
+            self.peripheralNames.append(peripheral.name ?? "unnamed device")
+        }
+    }
+}
 
 struct ContentView: View {
+    @ObservedObject private var bluetoothViewModel = BluetoothViewModel()
     @State var percent: CGFloat = 0
     @State var text: String = "(Placeholder)"
+    @State var menuState: CGFloat = 0
     
     func homePressed() {
         text = "Home"
+        menuState = 1
     }
     
     func infoPressed() {
         text = "Info"
+        menuState = 2
     }
     
     func dangersPressed() {
         text = "Dangers"
+        menuState = 3
     }
     
     func UVPressed() {
         text = "UV Index"
+        menuState = 4
     }
     
     var body: some View {
@@ -79,6 +113,14 @@ struct ContentView: View {
                     
                     Text(text)
                         .font(.system(size: 42, weight: .bold, design: .default))
+                    
+                    if(menuState == 1) {
+                        NavigationView {
+                            List(bluetoothViewModel.peripheralNames, id: \.self) { peripheral in Text(peripheral) }
+                                .navigationTitle("Bluetooth Devices")
+                                .navigationBarTitleDisplayMode(.inline)
+                        }
+                    }
                 }
                 
                 ZStack {
